ba4d253bb4e93de50602e251098ccb2ce78da7b8
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-10-08 23:29:07 +0900

    [Test] token refresh test

--- src/main/java/kr/sols/auth/jwt/TokenProvider.java
+++ src/main/java/kr/sols/auth/jwt/TokenProvider.java
@@ -34,7 +34,7 @@ public class TokenProvider {
     @Value("${jwt.key}")
     private String key;
     private SecretKey secretKey;
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L;  // 30분
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 10L;  // 30분
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60L * 24 * 7;  // 7일
 
     private static final String KEY_ROLE = "role";
