d710d87ce6e5cf747687e9e22756675e9125f089
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-12-09 18:48:19 +0900

    [Fix] 국내기업 검색 오류 해결

--- src/main/java/kr/sols/domain/company/repository/CompanyRepository.java
+++ src/main/java/kr/sols/domain/company/repository/CompanyRepository.java
@@ -32,10 +32,11 @@ public interface CompanyRepository extends JpaRepository<Company, UUID> {
     @Query(value = "SELECT * FROM company c " +
             "WHERE (c.company_name LIKE CONCAT(:searchTerm, '%') " +
             "OR EXISTS (SELECT 1 FROM JSON_TABLE(c.search_terms, '$[*]' COLUMNS(term VARCHAR(255) PATH '$')) AS jt " +
-            "WHERE jt.term LIKE CONCAT(:searchTerm, '%')) " +
+            "WHERE jt.term LIKE CONCAT(:searchTerm, '%'))) " +
             "ORDER BY c.company_name",
             nativeQuery = true)
     List<Company> searchAllCompanyByTerm(@Param("searchTerm") String searchTerm);
+
 
     @Query(value = "SELECT * FROM company c " +
             "WHERE c.is_public IS TRUE " +
