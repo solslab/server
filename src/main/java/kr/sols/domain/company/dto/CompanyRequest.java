82ec4bb58fdd8c1293e1c19d03f4610d264cdca4
gyujh630 <rbwjrbxo@ajou.ac.kr>
2024-12-24 16:00:06 +0900

    [Fix] isPublic 필드 카멜케이스 처리

--- src/main/java/kr/sols/domain/company/dto/CompanyRequest.java
+++ src/main/java/kr/sols/domain/company/dto/CompanyRequest.java
@@ -1,5 +1,6 @@
 package kr.sols.domain.company.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -15,5 +16,6 @@ public class CompanyRequest {
     private String companyName;
     private List<String> industryType;
     private List<String> searchTerms;
+    @JsonProperty("is_public")
     private boolean isPublic;
 }
