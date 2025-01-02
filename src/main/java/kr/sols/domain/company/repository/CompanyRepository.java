4d650e0bc4a815b4aa9471e7f95b20c7450ab8be
gyujh630 <rbwjrbxo@ajou.ac.kr>
2025-01-02 18:39:58 +0900

    [Fix] searchTerm 검색 오류 해결

--- src/main/java/kr/sols/domain/company/repository/CompanyRepository.java
+++ src/main/java/kr/sols/domain/company/repository/CompanyRepository.java
@@ -23,8 +23,10 @@ public interface CompanyRepository extends JpaRepository<Company, UUID> {
     @Query(value = "SELECT * FROM company c " +
             "WHERE (c.company_name LIKE CONCAT(:searchTerm, '%') " +
             "AND c.is_public IS TRUE) " +
-            "OR EXISTS (SELECT 1 FROM JSON_TABLE(c.search_terms, '$[*]' COLUMNS(term VARCHAR(255) PATH '$')) AS jt " +
-            "WHERE jt.term LIKE CONCAT(:searchTerm, '%')) " +
+            "OR (c.is_public IS TRUE AND EXISTS ( " +
+            "    SELECT 1 FROM JSON_TABLE(c.search_terms, '$[*]' COLUMNS(term VARCHAR(255) PATH '$')) AS jt " +
+            "    WHERE jt.term LIKE CONCAT(:searchTerm, '%') " +
+            ")) " +
             "ORDER BY c.company_name",
             nativeQuery = true)
     List<Company> searchCompanyByTerm(@Param("searchTerm") String searchTerm);
@@ -42,8 +44,10 @@ public interface CompanyRepository extends JpaRepository<Company, UUID> {
     @Query(value = "SELECT * FROM company c " +
             "WHERE (c.company_name LIKE CONCAT(:searchTerm, '%') " +
             "AND c.is_public IS FALSE) " +
-            "OR EXISTS (SELECT 1 FROM JSON_TABLE(c.search_terms, '$[*]' COLUMNS(term VARCHAR(255) PATH '$')) AS jt " +
-            "WHERE jt.term LIKE CONCAT(:searchTerm, '%')) " +
+            "OR (c.is_public IS FALSE AND EXISTS ( " +
+            "    SELECT 1 FROM JSON_TABLE(c.search_terms, '$[*]' COLUMNS(term VARCHAR(255) PATH '$')) AS jt " +
+            "    WHERE jt.term LIKE CONCAT(:searchTerm, '%') " +
+            ")) " +
             "ORDER BY c.company_name",
             nativeQuery = true)
     List<Company> searchPrivateCompanyByTerm(@Param("searchTerm") String searchTerm);
