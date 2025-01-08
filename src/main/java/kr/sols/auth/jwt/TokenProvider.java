37d76af6eafb22ddb03311dbeb818db0fb586107
gyujh630 <rbwjrbxo@ajou.ac.kr>
2025-01-08 18:53:22 +0900

    [Chore] 토큰 만료시간 조정

--- src/main/java/kr/sols/auth/jwt/TokenProvider.java
+++ src/main/java/kr/sols/auth/jwt/TokenProvider.java
@@ -34,7 +34,7 @@ public class TokenProvider {
     @Value("${jwt.key}")
     private String key;
     private SecretKey secretKey;
-    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 2L;
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30L;
     private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60L * 24 * 7;
 
     private static final String KEY_ROLE = "role";
